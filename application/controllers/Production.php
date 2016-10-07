7b401f429aa89a3897f93458e52a2eb80f670822
Wilson Lau <lauwilson90@gmail.com>
2016-10-07 15:18:30 -0700

    Add logic for Production Controller

--- application/controllers/Production.php
+++ application/controllers/Production.php
@@ -3,29 +3,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Production extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('recipesModel');
+		$this->load->model('suppliesModel');
+	}
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 * Index Page for the Production controller.
 	 */
 	public function index()
 	{
-		$this->getRecipes();
-		$this->load->view('production_view');
+		$recipes = $this->getViewData();
+		$this->data['recipes'] = $recipes;
+		$this->load->view('production_view', $this->data);
+	}
+
+
+	public function getViewData() {
+		$recipes = $this->getRecipes();
+		foreach ($recipes as &$recipe) {
+			foreach ($recipe['ingredients'] as &$ingredient) {
+				$ingredient['amt_in_stock'] = $this->getSupplyCount($ingredient['ingredient']);
+			}
+		}
+		return $recipes;
 	}
 
 	public function getRecipes() {
-		$this->load->model('recipesModel');
-		$receipes = $this->recipesModel->all();
+		$recipes = $this->recipesModel->all();
+		return $recipes;
+	}
+
+	public function getSupplyCount($code) {
+		$supplyCount = $this->suppliesModel->singleSupply($code)['quantityOnHand'];
+		return $supplyCount;
 	}
 }
