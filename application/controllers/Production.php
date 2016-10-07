848ea578dd730b5329df2fc7a38363252158428f
Wilson Lau <lauwilson90@gmail.com>
2016-10-07 15:27:53 -0700

    Add a boolean to Production view model to indicate whether the recipe can be produced.

--- application/controllers/Production.php
+++ application/controllers/Production.php
@@ -18,13 +18,17 @@ class Production extends CI_Controller {
 		$this->load->view('production_view', $this->data);
 	}
 
-
 	public function getViewData() {
 		$recipes = $this->getRecipes();
 		foreach ($recipes as &$recipe) {
+			$can_produce = TRUE;
 			foreach ($recipe['ingredients'] as &$ingredient) {
 				$ingredient['amt_in_stock'] = $this->getSupplyCount($ingredient['ingredient']);
+				if ($ingredient['amt_in_stock'] < $ingredient['amount']) {
+					$can_produce = FALSE;
+				}
 			}
+			$recipe['can_produce'] = $can_produce;
 		}
 		return $recipes;
 	}
